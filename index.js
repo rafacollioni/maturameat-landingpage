59a46f9f2226ba91764a51a6573e09565f73a6c5
rafacollioni <rafa.collioni@gmail.com>
2023-01-29 20:45:56 -0300

    navbar e introdução da página pronto

---
+++ index.js
@@ -0,0 +1,15 @@
+const menu_btn = document.querySelector('.hamburguer'),
+  mobile_nav = document.querySelector('.mobile-nav'),
+  menuItens = document.querySelectorAll('.nav-anchor');
+
+for (let i = 0; i < menuItens.length; i++) {
+  menuItens[i].addEventListener('click', function menuClose() {
+    menu_btn.classList.toggle('is-active');
+    mobile_nav.classList.toggle('is-active');
+  });
+}
+
+menu_btn.addEventListener('click', function menuToggle() {
+  menu_btn.classList.toggle('is-active');
+  mobile_nav.classList.toggle('is-active');
+});
